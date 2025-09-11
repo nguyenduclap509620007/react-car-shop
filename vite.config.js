48cbc4f500d524fcbd2b4c488987b699a3a6d678
nguyenduclap509620007 <lap0967513058@gmail.com>
2025-09-11 10:48:22 +0700

    update vite.config.js: set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-car-shop/'  // 👈 phải trùng tên repo của bạn
 })
