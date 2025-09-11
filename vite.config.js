014ba4c364d72505fd96f56bfbb9ae2597656766
nguyenduclap509620007 <lap0967513058@gmail.com>
2025-09-11 10:52:47 +0700

    fix vite.config.js base config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-car-shop/'  // 👈 phải trùng tên repo của bạn
+  base: '/react-car-shop/' // 👈 phải trùng tên repo
 })
